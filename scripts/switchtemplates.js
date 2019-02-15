bdb40f60e47f1ffc0b6f5ab13c4506f7b6b96552
Alessandro Buonocore <47116802+notAlessandro@users.noreply.github.com>
2019-02-16 00:27:36 +0100

    progetto iniziato
    prototipo iniziato

---
+++ scripts/switchtemplates.js
@@ -0,0 +1,25 @@
+var sequence = 1;
+$( document ).ready(function() 
+{
+	$('#btn1, #btn2, #btn3').click(function(event)
+	{
+		console.log("switch");
+		switch(sequence)
+		{
+			case 1:
+				// descrizione a sinistra, immagine a destra
+				console.log("descrizione a sinistra, immagine a destra");
+				sequence = 2;
+			break;
+
+			case 2:
+				// descrizione a destra, immagine a sinistra
+				console.log("descrizione a destra, immagine a sinistra");
+				sequence = 1;
+			break;
+		}
+
+		return false;
+
+	});
+});
